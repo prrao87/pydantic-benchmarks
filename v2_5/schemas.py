01bcc810b1b0a22fda6f11c470143247b7ab2884
Prashanth Rao <35005448+prrao87@users.noreply.github.com>
2023-11-13 15:25:12 -0500

    Add v2.5 benchmark

---
+++ v2_5/schemas.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel, ConfigDict, Field, model_validator
+
+
+class Wine(BaseModel):
+    model_config = ConfigDict(
+        populate_by_name=True,
+        str_strip_whitespace=True,
+    )
+
+    id: int
+    points: int
+    title: str
+    description: str | None
+    price: float | None
+    variety: str | None
+    winery: str | None
+    designation: str | None = Field(None, alias="vineyard")
+    country: str | None
+    province: str | None
+    region_1: str | None
+    region_2: str | None
+    taster_name: str | None
+    taster_twitter_handle: str | None
+
+    @model_validator(mode="before")
+    def _remove_unknowns(cls, values):
+        "Set other fields that have the value 'null' as None so that we can throw it away"
+        fields = ["designation", "province", "region_1", "region_2"]
+        for field in fields:
+            if not values.get(field) or values.get(field) == "null":
+                values[field] = None
+        return values
+
+    @model_validator(mode="before")
+    def _fill_country_unknowns(cls, values):
+        "Fill in missing country values with 'Unknown', as we always want this field to be queryable"
+        country = values.get("country")
+        if not country or country == "null":
+            values["country"] = "Unknown"
+        return values
+
+
+if __name__ == "__main__":
+    sample_data = {
+        "id": 45100,
+        "points": 85.0,  # Intentionally not an int to test coercion
+        "title": "Balduzzi 2012 Reserva Merlot (Maule Valley)",
+        "description": "Ripe in color and aromas, this chunky wine delivers heavy baked-berry and raisin aromas in front of a jammy, extracted palate. Raisin and cooked berry flavors finish plump, with earthy notes.",
+        "price": 10,  # Intentionally not a float to test coercion
+        "variety": "Merlot",
+        "winery": "Balduzzi",
+        "country": "null",  # Test null handling
+        "province": "Maule Valley",
+        "region_1": "null",  # Test null handling
+        "region_2": "null",
+        "taster_name": "Michael Schachner",
+        "taster_twitter_handle": "@wineschach",
+        "designation": "  The Vineyard  ",
+    }
+    wine = Wine(**sample_data)
+    from pprint import pprint
+
+    pprint(wine.model_dump(exclude_none=True, by_alias=True))
